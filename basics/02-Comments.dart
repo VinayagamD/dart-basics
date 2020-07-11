7b08c4d799427d0517484f2f4cc79967d6451d77
VinayagamD <vinayagam.d.ganesh@gmail.com>
2020-07-12 03:04:19 +0530

    Dart comment

---
+++ basics/02-Comments.dart
@@ -0,0 +1,17 @@
+/*
+    Multiline comment
+ */
+
+void main() {
+//  This is my first line of code
+  print('Hello World'); // This is another comment...
+
+  print('This is my first application');
+
+  // Performing arithmetic operation
+  print(4 - 1);
+  print(12 / 4);
+
+  // Printing boolean value
+  print(true);
+}
--- basics/Basics.dart
+++ basics/Basics.dart
@@ -1,6 +1,6 @@
 void main() {
-  print("Hello World");
-  print("This is my first application");
+  print('Hello World');
+  print('This is my first application');
   print(4-1);
   print(12/4);
   print(true);
